e11a37cf959ef17f3ab6387db3186cf387d8dfb8
Suhael Farouk <suhaelfarouk@gmail.com>
2022-09-22 16:53:12 +0530

    Data Types

---
+++ dataTypes.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+   int age = 40;
+   double gpa = 3.6;
+   char grade = 'A';
+   char phrase[] = "Giraffe Academy";
+
+   printf(" %d \n %f \n %c \n %s \n",age, gpa, grade, phrase); 
+   printf("My favorite word is %s and my favorite number is %d \n", "Giraffe", 100);
+   
+   return 0;
+}
